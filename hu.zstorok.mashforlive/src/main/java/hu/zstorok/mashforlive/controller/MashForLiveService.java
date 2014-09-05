da772981a104e9286569711010dcd603227a0765
Zsolt Török <zsolt@zsolttorok.com>
2014-09-06 00:33:34 +0200

    Initial implementation of the project ZIP download.

--- hu.zstorok.mashforlive/src/main/java/hu/zstorok/mashforlive/controller/MashForLiveService.java
+++ hu.zstorok.mashforlive/src/main/java/hu/zstorok/mashforlive/controller/MashForLiveService.java
@@ -24,6 +24,7 @@ import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -35,112 +36,141 @@ import com.google.common.io.ByteStreams;
 import com.google.common.io.Files;
 
 /**
- * REST controller implementation. 
+ * REST controller implementation.
  * 
  * @author zstorok
  */
 @RestController
 public class MashForLiveService {
-	
-    private static final String SAMPLES_FOLDER_RELATIVE_PATH = "Samples/";
+
+	private static final String ALS_FILE_RELATIVE_PATH = "mash_for_live.als";
+	private static final String SAMPLES_FOLDER_RELATIVE_PATH = "Samples/";
+
 	@Autowired
-    private SoundCloudClient soundCloudClient;
-    @Autowired
-    private EchoNestClient echoNestClient;
-    @Autowired
-    private LiveSetGenerator liveSetGenerator;
-    @Autowired
-    private ILiveSetBuilder liveSetBuilder;
+	private SoundCloudClient soundCloudClient;
+	@Autowired
+	private EchoNestClient echoNestClient;
+	@Autowired
+	private LiveSetGenerator liveSetGenerator;
+	@Autowired
+	private ILiveSetBuilder liveSetBuilder;
 
-    @RequestMapping(value="/service/zip", method=RequestMethod.POST, produces="application/octet-stream")
-    public void getProjectZip(String soundCloudTrackId, HttpServletResponse response) {
-    	File projectDirectory = Files.createTempDir();
-    	
-    	// get track data from SoundCloud
-    	JsonNode soundCloudTrack = soundCloudClient.getTrackAsJsonNode(soundCloudTrackId);
-    	String soundCloudTrackDownloadUrl = soundCloudTrack.get("download_url").asText() + "?consumer_key=" + SoundCloudConstants.CONSUMER_KEY;
-    	
-    	try {
-    		// download track from SoundCloud
-    		File audioFile = downloadAudioFile(projectDirectory, soundCloudTrackDownloadUrl);
-    		
-        	// upload track to EchoNest
-        	JsonNode trackUploadResponse = echoNestClient.uploadTrack(soundCloudTrackDownloadUrl);
-        	String echoNestTrackId = trackUploadResponse.get("response").get("track").get("id").asText();
-        	
-        	// get EchoNest track audio summary
-        	JsonNode echoNestTrackAudioSummary = echoNestClient.getTrackAudioSummaryAsJsonNode(echoNestTrackId);
-        	JsonNode echoNestTrackAudioSummaryResponse = echoNestTrackAudioSummary.get("response");
-        	String echoNestAnalysisUrl = echoNestTrackAudioSummaryResponse.get("track").get("audio_summary").get("analysis_url").asText();
-        	// get EchoNest track analysis data
-    		JsonNode echoNestAnalysis = echoNestClient.getAnalysisAsJsonNode(
-    				echoNestAnalysisUrl, echoNestTrackId);
+	@RequestMapping(value = "/service/zip", method = RequestMethod.POST, produces = "application/octet-stream")
+	public void getProjectZip(String soundCloudTrackId, HttpServletResponse response) throws NotDownloadableException,
+			DownloadFailedException {
+		File projectDirectory = Files.createTempDir();
 
-        	LiveSet liveSet = liveSetBuilder.build(echoNestAnalysis);
-    		byte[] alsBytes = liveSetGenerator.generateAls(liveSet);
+		// get track data from SoundCloud
+		JsonNode soundCloudTrack = soundCloudClient.getTrackAsJsonNode(soundCloudTrackId);
+		String soundCloudTrackDownloadUrl = soundCloudTrack.get("download_url").asText() + "?consumer_key="
+				+ SoundCloudConstants.CONSUMER_KEY;
 
-    		// create ZIP
-    		ZipOutputStream zipOutputStream = new ZipOutputStream(response.getOutputStream());
-    		zipOutputStream.putNextEntry(new ZipEntry(SAMPLES_FOLDER_RELATIVE_PATH));
-    		// add audio file to ZIP
-    		ZipEntry audioFileEntry = new ZipEntry(SAMPLES_FOLDER_RELATIVE_PATH + audioFile.getName());
+		// download track from SoundCloud
+		File audioFile = downloadAudioFile(projectDirectory, soundCloudTrackDownloadUrl);
+
+		// upload track to EchoNest
+		JsonNode trackUploadResponse = echoNestClient.uploadTrack(soundCloudTrackDownloadUrl);
+		String echoNestTrackId = trackUploadResponse.get("response").get("track").get("id").asText();
+
+		// get EchoNest track audio summary
+		JsonNode echoNestTrackAudioSummary = echoNestClient.getTrackAudioSummaryAsJsonNode(echoNestTrackId);
+		JsonNode echoNestTrackAudioSummaryResponse = echoNestTrackAudioSummary.get("response");
+		String echoNestAnalysisUrl = echoNestTrackAudioSummaryResponse.get("track").get("audio_summary")
+				.get("analysis_url").asText();
+		// get EchoNest track analysis data
+		JsonNode echoNestAnalysis = echoNestClient.getAnalysisAsJsonNode(echoNestAnalysisUrl, echoNestTrackId);
+
+		LiveSet liveSet = liveSetBuilder.build(echoNestAnalysis);
+		byte[] alsBytes = liveSetGenerator.generateAls(liveSet);
+
+		// create ZIP
+		ZipOutputStream zipOutputStream = null;
+		try {
+			zipOutputStream = new ZipOutputStream(response.getOutputStream());
+			zipOutputStream.putNextEntry(new ZipEntry(SAMPLES_FOLDER_RELATIVE_PATH));
+			// add audio file to ZIP
+			ZipEntry audioFileEntry = new ZipEntry(SAMPLES_FOLDER_RELATIVE_PATH + audioFile.getName());
 			zipOutputStream.putNextEntry(audioFileEntry);
 			FileInputStream audioFileInputStream = new FileInputStream(audioFile);
 			ByteStreams.copy(audioFileInputStream, zipOutputStream);
 			// add .als to zip
-			ZipEntry alsFileEntry = new ZipEntry("mash_for_live.als");
+			ZipEntry alsFileEntry = new ZipEntry(ALS_FILE_RELATIVE_PATH);
 			zipOutputStream.putNextEntry(alsFileEntry);
 			zipOutputStream.write(alsBytes);
-			zipOutputStream.closeEntry();
-			zipOutputStream.close();
-    	} catch (IOException e) {
-    		// TODO Auto-generated catch block
-    		e.printStackTrace();
-    	}
-    }
+		} catch (IOException e) {
+			throw new DownloadFailedException("Error when downloading project ZIP file.");
+		} finally {
+			if (zipOutputStream != null) {
+				try {
+					zipOutputStream.closeEntry();
+					zipOutputStream.close();
+				} catch (IOException e) {
+					// ignore
+				}
+			}
+		}
+	}
 
-	private File downloadAudioFile(File projectDirectory, String soundCloudTrackDownloadUrl) throws IOException {
+	private File downloadAudioFile(File projectDirectory, String soundCloudTrackDownloadUrl)
+			throws DownloadFailedException {
 		CloseableHttpClient httpClient = HttpClients.createDefault();
-		CloseableHttpResponse httpResponse = httpClient.execute(new HttpGet(soundCloudTrackDownloadUrl));
-		// TODO: check response status
-		String audioFileName = getAudioFileName(httpResponse);
-		byte[] inputBytes = EntityUtils.toByteArray(httpResponse.getEntity());
-		File audioFile = new File(projectDirectory, audioFileName);
-		audioFile.createNewFile();
-		System.out.println(audioFile.getAbsolutePath());
-		FileOutputStream audioFileOutputStream = new FileOutputStream(audioFile);
-		ByteStreams.copy(new ByteArrayInputStream(inputBytes), audioFileOutputStream);
-		audioFileOutputStream.close();
-		return audioFile;
+		try {
+			CloseableHttpResponse httpResponse = httpClient.execute(new HttpGet(soundCloudTrackDownloadUrl));
+			int statusCode = httpResponse.getStatusLine().getStatusCode();
+			if (statusCode != 200) {
+				throw new DownloadFailedException("Error when downloading track from SoundCloud; status code: "
+						+ statusCode);
+			}
+			String audioFileName = getAudioFileName(httpResponse);
+			byte[] inputBytes = EntityUtils.toByteArray(httpResponse.getEntity());
+			FileOutputStream audioFileOutputStream = null;
+			File audioFile = null;
+			try {
+				audioFile = new File(projectDirectory, audioFileName);
+				audioFile.createNewFile();
+				audioFileOutputStream = new FileOutputStream(audioFile);
+				ByteStreams.copy(new ByteArrayInputStream(inputBytes), audioFileOutputStream);
+			} catch (IOException e) {
+				throw new DownloadFailedException("Error when writing audio file downloaded from SoundCloud.");
+			} finally {
+				if (audioFileOutputStream != null) {
+					audioFileOutputStream.close();
+				}
+			}
+			return audioFile;
+		} catch (IOException e) {
+			throw new DownloadFailedException("Error when downloading track from SoundCloud.");
+		}
 	}
-    
+
 	private String getAudioFileName(CloseableHttpResponse httpResponse) {
-		return httpResponse.getHeaders("Content-Disposition")[0].getElements()[0].getParameterByName("filename").getValue();
+		return httpResponse.getHeaders("Content-Disposition")[0].getElements()[0].getParameterByName("filename")
+				.getValue();
 	}
 
-	@RequestMapping(value="/service/als", method=RequestMethod.POST, produces="application/octet-stream")
-	public @ResponseBody byte[] getAbletonLiveSet(String soundCloudTrackId)
-			throws NotDownloadableException {
-    	// get track data from SoundCloud
-    	JsonNode soundCloudTrack = soundCloudClient.getTrackAsJsonNode(soundCloudTrackId);
-    	if (soundCloudTrack.get("download_url") == null) {
+	@RequestMapping(value = "/service/als", method = RequestMethod.POST, produces = "application/octet-stream")
+	public @ResponseBody byte[] getAbletonLiveSet(String soundCloudTrackId) throws NotDownloadableException {
+		// get track data from SoundCloud
+		JsonNode soundCloudTrack = soundCloudClient.getTrackAsJsonNode(soundCloudTrackId);
+		if (soundCloudTrack.get("download_url") == null) {
 			throw new NotDownloadableException();
-    	}
-    	String soundCloudTrackDownloadLink = soundCloudTrack.get("download_url").asText();
-    	
-    	// upload track to EchoNest
-    	JsonNode trackUploadResponse = echoNestClient.uploadTrack(soundCloudTrackDownloadLink + "?consumer_key=" + SoundCloudConstants.CONSUMER_KEY);
-    	String echoNestTrackId = trackUploadResponse.get("response").get("track").get("id").asText();
-    	
-    	// get EchoNest track audio summary
-    	JsonNode echoNestTrackAudioSummary = echoNestClient.getTrackAudioSummaryAsJsonNode(echoNestTrackId);
-    	JsonNode echoNestTrackAudioSummaryResponse = echoNestTrackAudioSummary.get("response");
-    	String echoNestAnalysisUrl = echoNestTrackAudioSummaryResponse.get("track").get("audio_summary").get("analysis_url").asText();
-    	// get EchoNest track analysis data
-		JsonNode echoNestAnalysis = echoNestClient.getAnalysisAsJsonNode(
-				echoNestAnalysisUrl, echoNestTrackId);
+		}
+		String soundCloudTrackDownloadLink = soundCloudTrack.get("download_url").asText();
 
-    	LiveSet liveSet = liveSetBuilder.build(echoNestAnalysis);
+		// upload track to EchoNest
+		JsonNode trackUploadResponse = echoNestClient.uploadTrack(soundCloudTrackDownloadLink + "?consumer_key="
+				+ SoundCloudConstants.CONSUMER_KEY);
+		String echoNestTrackId = trackUploadResponse.get("response").get("track").get("id").asText();
+
+		// get EchoNest track audio summary
+		JsonNode echoNestTrackAudioSummary = echoNestClient.getTrackAudioSummaryAsJsonNode(echoNestTrackId);
+		JsonNode echoNestTrackAudioSummaryResponse = echoNestTrackAudioSummary.get("response");
+		String echoNestAnalysisUrl = echoNestTrackAudioSummaryResponse.get("track").get("audio_summary")
+				.get("analysis_url").asText();
+		// get EchoNest track analysis data
+		JsonNode echoNestAnalysis = echoNestClient.getAnalysisAsJsonNode(echoNestAnalysisUrl, echoNestTrackId);
+
+		LiveSet liveSet = liveSetBuilder.build(echoNestAnalysis);
 		return liveSetGenerator.generateAls(liveSet);
 	}
 
@@ -155,4 +185,19 @@ public class MashForLiveService {
 			super(arg0);
 		}
 	}
+
+	@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "Track download failed")
+	@SuppressWarnings("serial")
+	static class DownloadFailedException extends Exception {
+		public DownloadFailedException() {
+			super();
+		}
+
+		public DownloadFailedException(String arg0) {
+			super(arg0);
+		}
+	}
+	
+	@ExceptionHandler(value=Exception.class)
+	public void handleGenericException() {}
 }
