ff38097094760c6181389b23c75ac753d08d03cf
Zsolt Török <zsolt@zsolttorok.com>
2014-09-05 23:24:05 +0200

    Adding half-baked ZIP download implementation to avoid more conflicts.

--- hu.zstorok.mashforlive/src/main/java/hu/zstorok/mashforlive/controller/MashForLiveService.java
+++ hu.zstorok.mashforlive/src/main/java/hu/zstorok/mashforlive/controller/MashForLiveService.java
@@ -7,6 +7,21 @@ import hu.zstorok.mashforlive.client.EchoNestClient;
 import hu.zstorok.mashforlive.client.SoundCloudClient;
 import hu.zstorok.mashforlive.client.SoundCloudConstants;
 
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +31,8 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.io.ByteStreams;
+import com.google.common.io.Files;
 
 /**
  * REST controller implementation. 
@@ -25,7 +42,8 @@ import com.fasterxml.jackson.databind.JsonNode;
 @RestController
 public class MashForLiveService {
 	
-    @Autowired
+    private static final String SAMPLES_FOLDER_RELATIVE_PATH = "Samples/";
+	@Autowired
     private SoundCloudClient soundCloudClient;
     @Autowired
     private EchoNestClient echoNestClient;
@@ -33,6 +51,72 @@ public class MashForLiveService {
     private LiveSetGenerator liveSetGenerator;
     @Autowired
     private ILiveSetBuilder liveSetBuilder;
+
+    @RequestMapping(value="/service/zip", method=RequestMethod.POST, produces="application/octet-stream")
+    public void getProjectZip(String soundCloudTrackId, HttpServletResponse response) {
+    	File projectDirectory = Files.createTempDir();
+    	
+    	// get track data from SoundCloud
+    	JsonNode soundCloudTrack = soundCloudClient.getTrackAsJsonNode(soundCloudTrackId);
+    	String soundCloudTrackDownloadUrl = soundCloudTrack.get("download_url").asText() + "?consumer_key=" + SoundCloudConstants.CONSUMER_KEY;
+    	
+    	try {
+    		// download track from SoundCloud
+    		File audioFile = downloadAudioFile(projectDirectory, soundCloudTrackDownloadUrl);
+    		
+        	// upload track to EchoNest
+        	JsonNode trackUploadResponse = echoNestClient.uploadTrack(soundCloudTrackDownloadUrl);
+        	String echoNestTrackId = trackUploadResponse.get("response").get("track").get("id").asText();
+        	
+        	// get EchoNest track audio summary
+        	JsonNode echoNestTrackAudioSummary = echoNestClient.getTrackAudioSummaryAsJsonNode(echoNestTrackId);
+        	JsonNode echoNestTrackAudioSummaryResponse = echoNestTrackAudioSummary.get("response");
+        	String echoNestAnalysisUrl = echoNestTrackAudioSummaryResponse.get("track").get("audio_summary").get("analysis_url").asText();
+        	// get EchoNest track analysis data
+    		JsonNode echoNestAnalysis = echoNestClient.getAnalysisAsJsonNode(
+    				echoNestAnalysisUrl, echoNestTrackId);
+
+        	LiveSet liveSet = liveSetBuilder.build(echoNestAnalysis);
+    		byte[] alsBytes = liveSetGenerator.generateAls(liveSet);
+
+    		// create ZIP
+    		ZipOutputStream zipOutputStream = new ZipOutputStream(response.getOutputStream());
+    		zipOutputStream.putNextEntry(new ZipEntry(SAMPLES_FOLDER_RELATIVE_PATH));
+    		// add audio file to ZIP
+    		ZipEntry audioFileEntry = new ZipEntry(SAMPLES_FOLDER_RELATIVE_PATH + audioFile.getName());
+			zipOutputStream.putNextEntry(audioFileEntry);
+			FileInputStream audioFileInputStream = new FileInputStream(audioFile);
+			ByteStreams.copy(audioFileInputStream, zipOutputStream);
+			// add .als to zip
+			ZipEntry alsFileEntry = new ZipEntry("mash_for_live.als");
+			zipOutputStream.putNextEntry(alsFileEntry);
+			zipOutputStream.write(alsBytes);
+			zipOutputStream.closeEntry();
+			zipOutputStream.close();
+    	} catch (IOException e) {
+    		// TODO Auto-generated catch block
+    		e.printStackTrace();
+    	}
+    }
+
+	private File downloadAudioFile(File projectDirectory, String soundCloudTrackDownloadUrl) throws IOException {
+		CloseableHttpClient httpClient = HttpClients.createDefault();
+		CloseableHttpResponse httpResponse = httpClient.execute(new HttpGet(soundCloudTrackDownloadUrl));
+		// TODO: check response status
+		String audioFileName = getAudioFileName(httpResponse);
+		byte[] inputBytes = EntityUtils.toByteArray(httpResponse.getEntity());
+		File audioFile = new File(projectDirectory, audioFileName);
+		audioFile.createNewFile();
+		System.out.println(audioFile.getAbsolutePath());
+		FileOutputStream audioFileOutputStream = new FileOutputStream(audioFile);
+		ByteStreams.copy(new ByteArrayInputStream(inputBytes), audioFileOutputStream);
+		audioFileOutputStream.close();
+		return audioFile;
+	}
+    
+	private String getAudioFileName(CloseableHttpResponse httpResponse) {
+		return httpResponse.getHeaders("Content-Disposition")[0].getElements()[0].getParameterByName("filename").getValue();
+	}
 
 	@RequestMapping(value="/service/als", method=RequestMethod.POST, produces="application/octet-stream")
 	public @ResponseBody byte[] getAbletonLiveSet(String soundCloudTrackId)
