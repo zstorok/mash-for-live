1f8272295b2f171bb3546582d597c728b2cec2c3
Zsolt Török <zsolt@zsolttorok.com>
2014-09-06 11:10:57 +0200

    Added Live Set builder implementation with warp markers and clip looping

--- hu.zstorok.mashforlive/src/main/java/hu/zstorok/mashforlive/Application.java
+++ hu.zstorok.mashforlive/src/main/java/hu/zstorok/mashforlive/Application.java
@@ -1,7 +1,7 @@
 package hu.zstorok.mashforlive;
 
 import hu.zstorok.mashforlive.als.ILiveSetBuilder;
-import hu.zstorok.mashforlive.als.MultiTrackBarClipLiveSetBuilder;
+import hu.zstorok.mashforlive.als.LoopingMultiTrackBarClipLiveSetBuilder;
 import hu.zstorok.mashforlive.client.EchoNestClient;
 import hu.zstorok.mashforlive.client.SoundCloudClient;
 
@@ -24,7 +24,7 @@ public class Application {
 
     @Bean
     public ILiveSetBuilder getLiveSetBuilder() {
-		return new MultiTrackBarClipLiveSetBuilder();
+		return new LoopingMultiTrackBarClipLiveSetBuilder();
     }
     
 	public static void main(String[] args) {
---
+++ hu.zstorok.mashforlive/src/main/java/hu/zstorok/mashforlive/als/LoopingMultiTrackBarClipLiveSetBuilder.java
@@ -0,0 +1,65 @@
+package hu.zstorok.mashforlive.als;
+
+import hu.zstorok.mashforlive.als.Clip.ClipColor;
+import hu.zstorok.mashforlive.als.Clip.WarpMarker;
+import hu.zstorok.mashforlive.client.EchoNestAnalysis;
+import hu.zstorok.mashforlive.client.EchoNestAnalysis.Bar;
+import hu.zstorok.mashforlive.client.EchoNestAnalysis.Beat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+/**
+ * Ableton Live set builder implementation that distributes the clips created
+ * from each bar of a song on multiple tracks.
+ * 
+ * @author zstorok
+ */
+public class LoopingMultiTrackBarClipLiveSetBuilder implements ILiveSetBuilder {
+
+	private static final int NUMBER_OF_TRACKS = 8;
+
+	@Override
+	public LiveSet build(EchoNestAnalysis echoNestAnalysis, String sampleFileName) {
+		EchoNestAnalysis.Track track = echoNestAnalysis.getTrack();
+		int timeSignature = track.getTime_signature();
+		LiveSet liveSet = new LiveSet(track.getTempo());
+		List<Bar> bars = echoNestAnalysis.getBars();
+		int barCount = bars.size();
+		int clipsPerTrack = Math.floorDiv(barCount, NUMBER_OF_TRACKS) + 1;
+		int trackIndex = 1;
+		Track liveSetTrack = null;
+		int i = 0;
+		for (; i < barCount; i++) {
+			if (i % clipsPerTrack == 0) {
+				liveSetTrack = new Track(trackIndex, "track " + trackIndex);
+				liveSet.getTracks().add(liveSetTrack);
+				trackIndex++;
+			}
+			Bar bar = bars.get(i);
+			List<WarpMarker> warpMarkers = buildWarpMarkers(echoNestAnalysis);
+			// clip loop positions are in beats when the clip is warped
+			liveSetTrack.getClips().add(new Clip(i, "clip " + i, sampleFileName, i * timeSignature, (i + 1) * timeSignature, warpMarkers, 
+					ClipColor.random(), false));
+		}
+		// tracks with less clips need to be padded
+		int emptyClipsToAdd = clipsPerTrack - i % clipsPerTrack;
+		for (int j = 0; j < emptyClipsToAdd; j++) {
+			liveSetTrack.getClips().add(new Clip());
+		}
+		return liveSet;
+	}
+
+	private List<WarpMarker> buildWarpMarkers(EchoNestAnalysis echoNestAnalysis) {
+		List<Beat> beats = echoNestAnalysis.getBeats();
+		ArrayList<WarpMarker> warpMarkers = Lists.newArrayList();
+		for (int i = 0; i < beats.size(); i++) {
+			Beat beat = beats.get(i);
+			warpMarkers.add(new WarpMarker(beat.getStart(), i));
+		}
+		return warpMarkers;
+	}
+
+}
